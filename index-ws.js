9becc716327406f401efeea4338b3af1d173efbb
katsuikeda <128643018+katsuikeda@users.noreply.github.com>
2023-07-25 23:38:19 -0700

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -18,6 +18,18 @@ server.listen(3000, function () {
 	console.log("Listening on 3000");
 });
 
+// Catch the interrupt signal when the server is closing
+// Go thorough every single WebSocket connection, and close it
+// And shut down the db connection before the server actually closes
+process.on("SIGINT", () => {
+	wss.clients.forEach(function each(client) {
+		client.close();
+	});
+	server.close(() => {
+		shutdownDB();
+	});
+});
+
 /** Begin websocket */
 const WebSocketServer = require("ws").Server;
 
@@ -36,6 +48,12 @@ wss.on("connection", function connection(ws) {
 		ws.send("Welcome to my server");
 	}
 
+	// datetime(): sqlite3's built-in function for time
+	db.run(`
+    INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
 	ws.on("close", function close() {
 		wss.broadcast(`Current visitors ${numClients}`);
 		console.log("A client has disconnected");
@@ -47,3 +65,31 @@ wss.broadcast = function broadcast(data) {
 		client.send(data);
 	});
 };
+
+/** end web sockets */
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+// Make sure our database is set up before any queries are ready to go
+db.serialize(() => {
+	db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+	db.each("SELECT * FROM visitors", (err, row) => {
+		console.log(row);
+	});
+}
+
+// We need to close the database connection by the time the server is all done. We never want to leave a database connection open because it will just stay open persistently. Then you are going to run out of connections to your database.
+function shutdownDB() {
+	getCounts();
+	console.log("Shutting down db");
+	db.close();
+}
